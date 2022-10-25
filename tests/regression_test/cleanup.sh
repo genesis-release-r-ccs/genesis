cfcbbbfb5e12d87086a8ac24de57b70029564560
Chigusa Kobayashi <ckobayashi@riken.jp>
2022-10-25 17:40:17 +0900

    update regression tests (cleanup.sh) for Fugaku

--- tests/regression_test/cleanup.sh
+++ tests/regression_test/cleanup.sh
@@ -7,53 +7,63 @@
 # Usage:
 #    $ ./cleanup.sh
 
-rm test_atdyn/*/*/error      >& /dev/null
-rm test_atdyn/*/*/test       >& /dev/null
+rm test_atdyn/*/*/error*     >& /dev/null
+rm test_atdyn/*/*/test*      >& /dev/null
+rm -r test_atdyn/*/*/output.?*      >& /dev/null
 rm test_atdyn/br/*/qmmm.0/*.inp            >& /dev/null
 rm test_atdyn/br/*/qmmm.0/*qm.xyz          >& /dev/null
 rm test_atdyn/br/*/qmmm.0/*pc.xyz          >& /dev/null
 rm test_atdyn/br/*/qmmm.0/mm_charges.dat   >& /dev/null
 
-rm test_spdyn/*/*/error      >& /dev/null
-rm test_spdyn/*/*/test       >& /dev/null
+rm test_spdyn/*/*/error*     >& /dev/null
+rm test_spdyn/*/*/test*      >& /dev/null
+rm -r test_spdyn/*/*/output.?*      >& /dev/null
 
-rm test_remd_atdyn/*/error   >& /dev/null
+rm test_remd_atdyn/*/error*  >& /dev/null
 rm test_remd_atdyn/*/log     >& /dev/null
 rm test_remd_atdyn/*/test?   >& /dev/null
-rm test_remd_spdyn/*/error   >& /dev/null
+rm -r test_remd_atdyn/*/output.?*      >& /dev/null
+rm test_remd_spdyn/*/error*  >& /dev/null
 rm test_remd_spdyn/*/log     >& /dev/null
 rm test_remd_spdyn/*/test?   >& /dev/null
+rm -r test_remd_spdyn/*/output.?*      >& /dev/null
 
-rm test_rpath_atdyn/*/error  >& /dev/null
+rm test_rpath_atdyn/*/error* >& /dev/null
 rm test_rpath_atdyn/*/log    >& /dev/null
 rm test_rpath_atdyn/*/test*  >& /dev/null
 rm test_rpath_atdyn/tim*/dcd*          >& /dev/null
 rm test_rpath_atdyn/tim*/rst*          >& /dev/null
 rm test_rpath_atdyn/tim*/qmmm.*/*.inp           >& /dev/null
 rm test_rpath_atdyn/tim*/qmmm.*/mm_charges.dat  >& /dev/null
+rm -r test_rpath_atdyn/*/output.?*      >& /dev/null
 
-rm test_rpath_spdyn/*/error  >& /dev/null
+rm test_rpath_spdyn/*/error* >& /dev/null
 rm test_rpath_spdyn/*/log    >& /dev/null
 rm test_rpath_spdyn/*/test?  >& /dev/null
+rm -r test_rpath_spdyn/*/output.?*      >& /dev/null
 
-rm test_vib/*/error          >& /dev/null
+rm test_vib/*/error*         >& /dev/null
 rm test_vib/*/log            >& /dev/null
 rm test_vib/*/vib.minfo      >& /dev/null
 rm test_vib/*/test?          >& /dev/null
 rm test_vib/*/qmmm.?/*.inp   >& /dev/null
 rm -r test_vib/*/minfo.files >& /dev/null
+rm -r test_vib/*/output.?*      >& /dev/null
 
-rm test_gamd_atdyn/*/*/test     >& /dev/null
-rm test_gamd_atdyn/*/*/error    >& /dev/null
+rm test_gamd_atdyn/*/*/test*    >& /dev/null
+rm test_gamd_atdyn/*/*/error*   >& /dev/null
 rm test_gamd_atdyn/*/*/out.gamd >& /dev/null
+rm -r test_gamd_atdyn/*/*/output.?*      >& /dev/null
 
-rm test_gamd_spdyn/*/*/test     >& /dev/null
-rm test_gamd_spdyn/*/*/error    >& /dev/null
+rm test_gamd_spdyn/*/*/test*    >& /dev/null
+rm test_gamd_spdyn/*/*/error*   >& /dev/null
 rm test_gamd_spdyn/*/*/out.gamd >& /dev/null
+rm -r test_gamd_spdyn/*/*/output.?*      >& /dev/null
 
 rm test_fep/*/*/test*       >& /dev/null
-rm test_fep/*/*/error       >& /dev/null
+rm test_fep/*/*/error*      >& /dev/null
 rm test_fep/*/*/out*.fepout >& /dev/null
+rm -r test_fep/*/*/output.?*      >& /dev/null
 
 cd test_analysis
 ./cleanup.sh  >& /dev/null
